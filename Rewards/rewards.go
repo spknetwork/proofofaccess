929b86e6797c9022a0f5162e7b87107c96b5c53f
nathansenn <nathan.senn90@gmail.com>
2023-08-02 05:22:07 +0800

    Decreased the sleep time from 10 seconds to 4 seconds in the RunProofs function.

--- Rewards/rewards.go
+++ Rewards/rewards.go
@@ -86,7 +86,7 @@ func RunProofs() error {
 						if peerHash == cid {
 							fmt.Println("Running proof for peer: " + peer + " and CID: " + cid)
 							go RunProof(peer, cid)
-							time.Sleep(10 * time.Second)
+							time.Sleep(4 * time.Second)
 						}
 					}
 				}
