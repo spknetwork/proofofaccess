66564acdf1a88fc8c7c30809b725c735fd82b2e7
nathansenn <nathan.senn90@gmail.com>
2023-10-23 15:40:23 +0800

    Implement CID synchronization for 3speak nodes.

--- messaging/syncing.go
+++ messaging/syncing.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"proofofaccess/database"
+	"proofofaccess/ipfs"
 	"proofofaccess/localdata"
 	"proofofaccess/pubsub"
+	"strconv"
+	"time"
 )
 
 func SendSyncing(req Request) {
@@ -75,13 +79,114 @@ func ReceiveSynced(req Request) {
 }
 func SyncNode(req Request) {
 	fmt.Println("Syncing with " + req.User)
-	peerName := req.User
 	localdata.Lock.Lock()
 	Nodes[req.User] = true
-	localdata.PeerNames = localdata.RemoveDuplicates(append(localdata.PeerNames, peerName))
-	localdata.NodesStatus[req.User] = "Synced"
 	localdata.Lock.Unlock()
-	SendSynced(req)
-	fmt.Println("Synced with " + req.User)
-	Nodes[req.User] = false
+	fmt.Println("Requesting CIDS")
+	peerName := req.User
+	localdata.Lock.Lock()
+	localdata.PeerNames = localdata.RemoveDuplicates(append(localdata.PeerNames, peerName))
+	localdata.NodesStatus[req.User] = "Syncing"
+	localdata.Lock.Unlock()
+	fmt.Println("Requesting CIDS from " + req.User)
+	fmt.Println(req.Pins)
+	var myData map[string]interface{}
+
+	if localdata.WsPeers[req.User] != req.User && localdata.NodeType == 1 {
+		fmt.Println("Ipfs DownloadAndDecodeJSON")
+		err := ipfs.DownloadAndDecodeJSON(req.CID, &myData)
+		if err != nil {
+			localdata.Lock.Lock()
+			Nodes[req.User] = false
+			localdata.Lock.Unlock()
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+			RequestCIDS(req)
+			return
+		}
+	}
+
+	// Handle the chunks of allPins data
+	part, _ := strconv.Atoi(req.Part)
+	totalParts, _ := strconv.Atoi(req.TotalParts)
+	fmt.Println("Part", part, "of", totalParts)
+	var pins []string
+	err := json.Unmarshal([]byte(req.Pins), &pins)
+	if err != nil {
+		log.Println("Error unmarshalling pins:", err)
+		log.Println("Pins data:", req.Pins)
+		return
+	}
+	// Lock to safely read from shared data
+	localdata.Lock.Lock()
+	allPins := localdata.PeerCids[req.User]
+	localdata.Lock.Unlock()
+
+	// Create a map to use as a set for unique values
+	uniquePins := make(map[string]struct{})
+
+	// Populate the map with the existing pins
+	for _, pin := range allPins {
+		uniquePins[pin] = struct{}{}
+	}
+
+	// Add new pins to the map, automatically removing duplicates
+	for _, pin := range pins {
+		uniquePins[pin] = struct{}{}
+	}
+
+	// Convert the map keys back into a slice
+	allPins = make([]string, 0, len(uniquePins))
+	for pin := range uniquePins {
+		allPins = append(allPins, pin)
+	}
+
+	// Lock to safely write to shared data
+	localdata.Lock.Lock()
+	localdata.PeerCids[req.User] = allPins
+	localdata.PeerSyncSeed[req.Seed] = localdata.PeerSyncSeed[req.Seed] + 1
+	localdata.Lock.Unlock()
+	fmt.Println("Received", len(pins), "CIDs from", req.User, "(", part, "/", totalParts, ")")
+	// If this is the last chunk, sync the node
+	// Convert allPins to JSON
+	allPinsJson, err := json.Marshal(allPins)
+	if err != nil {
+		log.Println("Error marshalling allPins:", err)
+		return
+	}
+
+	// Save allPins to the database
+	database.Save([]byte("allPins:"+req.User), allPinsJson)
+	fmt.Println("Saved allPins to database")
+	syncSeed := localdata.PeerSyncSeed[req.Seed]
+	if syncSeed == totalParts {
+		localdata.Lock.Lock()
+		localdata.NodesStatus[req.User] = "Synced"
+		localdata.Lock.Unlock()
+	} else if syncSeed == 1 {
+		SendSyncing(req)
+	} else {
+		return
+	}
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("Checking if synced with " + req.User)
+			localdata.Lock.Lock()
+			nodeStatus := localdata.NodesStatus[req.User]
+			localdata.Lock.Unlock()
+			if nodeStatus == "Synced" {
+				fmt.Println("Synced with " + req.User)
+				SendSynced(req)
+				localdata.Lock.Lock()
+				Nodes[req.User] = false
+				localdata.Lock.Unlock()
+				break
+			} else if nodeStatus == "Failed" {
+				RequestCIDS(req)
+				break
+			}
+		}
+	}()
 }
